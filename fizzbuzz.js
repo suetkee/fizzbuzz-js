5809ea9e47df64110f2bf76da15c8b965c0ae719
Suet Kee <suet.kee@softwire.com>
2025-01-21 20:22:49 +0000

    Reverse

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,42 +1,90 @@
 
 
 // Here, we create our main function.
-function fizzbuzz() {
+function fizzbuzz(n) {
     
     console.log('Welcome to FizzBuzz!');
     // Put your code here...
      
     console.log('Hello World');
+    
 
-    for(let i=0; i<101; i++)
+    let Num = [];
+  
+    for(let i=1; i<(n+1); i++)
     {
+        let output = "";
        // console.log(i);
-       if(i%11==0){
-        console.log("Bong");
-       } else if((i%7==0 && i%5==0) ||(i%7==0 && i%3==0)){
-        console.log("FizzBang");
-       } else if(i%7==0){
-        console.log("Bang");
-       } else if(i%3==0 && i%5==0){
-        console.log('fizzbuzz');
-       }   else if(i%3==0){
-            console.log('fizz');
+       
+       if(i%3==0){
+        output += 'Fizz';
+       }
 
-        } else if(i%5==0){
-            console.log('buzz');
+       if(i%13==0){
+        if(output ==''){
+        output='Fezz';
+       } else {
+        output+='Fezz';
+       }
+    
+    }
+
+       if(i%5==0){
+        output += 'Buzz';
+       }
+       
+       if(i%7==0){
+        output+= 'Bang';
+       }
+
+       if(i%11==0){
+        if(output=='Fezz'){
+            output+='Bong';
+        }else {
+        output ='Bong';
         }
-         else {
-            console.log(i);
+       }
+
+       if(i%17==0){
+        if(output.length>4){
+           
+            //
+                          let segments = [];
+                for (let j = 0; j < output.length; j += 4) {
+                    segments.push(output.slice(j, j + 4));
+                }
+                
+                const RevSegments = segments.reverse();
+                let result ='';
+                for(let k=0; k<RevSegments.length; k++){
+                        result += RevSegments[k];
+                }
+                output = result;
+            }
+            
+          
+            //
         }
-        
+              
+       if(output !==''){
+        Num.push(output);
+       } else {
+        Num.push(i);
+       }
+              
 
     }
     
+   
+    console.log(Num);
+    console.log(Num[194]);
+    console.log(Num[142]);
+    console.log(Num[254]);
 
     
 
-    
+
 }
 
 // Now we run the main function...
-fizzbuzz();
+fizzbuzz(300);
